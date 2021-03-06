723f3008eeaaeb16668858436c87996ae7f48963
Your Name <you@example.com>
2021-03-06 05:51:22 -0800

    sdwertyjhgfdszx

--- js.js
+++ js.js
@@ -7,3 +7,16 @@ function f1(){
 }
 
 //+ ' ' clojivat stroki
+
+//for each
+//kotoriy primenyaet funksii 1 raz k ukazannomu elemantu
+/*
+let arr =['first element','second ',11, true,]
+arr.forEach(function(item ){
+    console.log(`element: ${item}`)
+})
+*/
+let b = [1,2,3,4,]
+b.forEach(function(elem,){
+    console.log(elem);
+})
