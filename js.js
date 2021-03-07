c4e49b80380b48ef2f3e1469054645ed460d676a
Your Name <you@example.com>
2021-03-07 02:29:45 -0800

    efbgsd

--- js.js
+++ js.js
@@ -5,9 +5,7 @@ function f1(){
   p.innerHTML += i+' ' ;
     }
 }
-
 //+ ' ' clojivat stroki
-
 //for each
 //kotoriy primenyaet funksii 1 raz k ukazannomu elemantu
 /*
@@ -16,7 +14,67 @@ arr.forEach(function(item ){
     console.log(`element: ${item}`)
 })
 */
+/*
 let b = [1,2,3,4,]
 b.forEach(function(elem,){
-    console.log(elem * 3);
+    console.log(elem + 113);
 })
+*/
+
+
+
+//for In schitaet    in daet kluchi 
+//for of znachenie
+//for let sozdaet peramennoe  ublechiveat na edinisu
+//for age ne otloichaetsya ot for of  prohogitsya po vsem massivam  i znachenie i kluch  ne vozvrashaet massive
+//for of po poverhnosti podhodit znachenie 
+//for in age of nuzno massive 
+// item znachenie elamenta
+//.puch dobavit elament
+//map sozdaet novyi massive
+
+
+
+function List2({items}){
+    let example = ["Aplle" , "pear","beer"]
+    let result = []
+    for (item of example){
+        result.push("1" +item +"!")
+    }
+    }*/
+
+
+
+
+
+//Map
+//perebirat massivy 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
